aecaa6358c43d55f041269b2319f0111c82ba909
Natan <nlidukhover@gmail.com>
2020-10-29 16:05:03 -0500

    Added struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
+use std::ptr;
 use std::fs::File;
 use std::path::Path;
 use std::ffi::c_void;
 use std::os::unix::io::{AsRawFd, RawFd};
 
-const INQ_REPLY_LEN: i32 = 96;
-const INQ_CMD_CODE: i32 = 0x12;
-const INQ_CMD_LEN: i32 = 6;
+const INQ_REPLY_LEN : u8 = 96;
+const INQ_CMD_CODE : u8 = 0x12;
+const INQ_CMD_LEN : u8 = 6;
+const SG_GET_VERSION_NUM : u32 = 0x2282;
+const SG_IO: u32 = 0x2285;
+const SG_DXFER_NONE : i32 = -1;
+const SG_DXFER_FROM_DEV : i32 = -3;
 
 extern "C" {
 	fn ioctl(fd: i32, request: u32, ...) -> i32;
@@ -14,80 +19,112 @@ extern "C" {
 
 #[repr(C)]
 struct sg_io_hdr {
-	interface_id:		i32,			// [i] 'S' for SCSI generic (required)
-	dxfer_direction:	i32,			// [i] data transfer direction
-	cmd_len:			u8,				// [i] SCSI command length ( <= 16 bytes)
-	mx_sb_len:			u8,				// [i] max length to write to sbp
-	iovec_count:		u16,			// [i] 0 implies no scatter gather
-	dxfer_len:			u32,			// [i] byte count of data transfer
-	dxferp:				*mut c_void,	// [i], [*io] points to data transfer memory or scatter gather list
-	cmdp:				*const u8,		// [i], [*i] points to command to perform
-	sbp:				*mut u8,		// [i], [*o] points to sense_buffer memory
-	timeout:			u32,			// [i] MAX_UINT->no timeout (unit: millisec)
-	flags:				u32,			// [i] 0 -> default, see SG_FLAG...
-	pack_id:			i32,			// [i->o] unused internally (normally)
-	usr_ptr:			*mut c_void,	// [i->o] unused internally
-	status:				u8,				// [o] scsi status
-	masked_status:		u8,				// [o] shifted, masked scsi status
-	msg_status:			u8,				// [o] messaging level data (optional)
-	sb_len_wr:			u8,				// [o] byte count actually written to sbp
-	host_status:		u16,			// [o] errors from host adapter
-	driver_status:		u16,			// [o] errors from software driver
-	resid:				i32,			// [o] dxfer_len - actual_transferred
-	duration:			u32,			// [o] time taken by cmd (unit: millisec)
-	info:				u32,			// [o] auxiliary information
+	interface_id:		i32,			// [i] guard field. Current implementations only accept "(int)'S'"
+	dxfer_direction:	i32,			// [i] direction of data transfer
+	cmd_len:			u8,				// [i] limits command length to 16 bytes
+	mx_sb_len:			u8,				// [i] maximum number of bytes of sense data that the driver can output via the sbp pointer
+	iovec_count:		u16,			// [i] if not sg driver and greater than zero then the SG_IO ioctl fails with errno set to EOPNOTSUPP
+	dxfer_len:			u32,			// [i] number of bytes of data to transfer to or from the device
+	dxferp:				*mut c_void,	// [i], [*io] pointer to (user space) data to transfer to (if reading from device) or transfer from (if writing to device)
+	cmdp:				*const u8,		// [i], [*i] pointer to SCSI command
+	sbp:				*mut u8,		// [i], [*o] pointer to user data area where no more than max_sb_len bytes of sense data from the device will be written if the SCSI status is CHECK CONDITION
+	timeout:			u32,			// [i] time in milliseconds that the SCSI mid-level will wait for a response
+	flags:				u32,			// [i] block layer SG_IO ioctl ignores this field; the sg driver uses it to request special services like direct IO or mmap-ed transfers
+	pack_id:			i32,			// [i->o] unused (for user space program tag)
+	usr_ptr:			*mut c_void,	// [i->o] unused (for user space pointer tag)
+	status:				u8,				// [o] SCSI command status, zero implies GOOD
+	masked_status:		u8,				// [o] logically: masked_status == ((status & 0x3e) >> 1). Old linux SCSI subsystem usage, deprecated.
+	msg_status:			u8,				// [o] SCSI parallel interface (SPI) message status (very old, deprecated)
+	sb_len_wr:			u8,				// [o] actual length of sense data (in bytes) output via sbp pointer
+	host_status:		u16,			// [o] error reported by the initiator (port). These are the "DID_*" error codes in scsi.h
+	driver_status:		u16,			// [o] bit mask: error and suggestion reported by the low level driver (LLD)
+	resid:				i32,			// [o] (dxfer_len - number_of_bytes_actually_transferred)
+	duration:			u32,			// [o] number of milliseconds that elapsed between when the command was injected into the SCSI mid level and the corresponding "done" callback was invoked
+	info:				u32,			// [o] bit mask indicating what was done (or not) and whether any error was detected
+}
+
+impl Default for sg_io_hdr {
+    fn default() -> Self {
+        sg_io_hdr {
+            interface_id: 		'S' as i32,
+			dxfer_direction: 	0,
+			cmd_len: 			0,
+			mx_sb_len: 			0,
+			iovec_count:		0,
+			dxfer_len: 			0,
+			dxferp: 			ptr::null_mut(),
+			cmdp: 				ptr::null_mut(),
+			sbp: 				ptr::null_mut(),
+			timeout: 			30000,
+			flags:				0,
+			pack_id:			0,
+			usr_ptr:			ptr::null_mut(),
+			status:				0,
+			masked_status:		0,
+			msg_status:			0,
+			sb_len_wr:			0,
+			host_status:		0,
+			driver_status:		0,
+			resid:				0,
+			duration:			0,
+			info:				0,
+        }
+    }
+}
+
+impl sg_io_hdr {
+    fn new() -> Self {
+        Default::default()
+    }
 }
 
 fn main() {
 	std::process::exit(match run_app() {
-        Ok(_) => {
+		Ok(_) => {
 			println!("GREAT SUCCESS!");
 			0
 		},
-        Err(err) => {
-            eprintln!("ERROR :: {:?}", err);
-            1
-        }
-    });
+		Err(err) => {
+			eprintln!("ERROR :: {:?}", err);
+			1
+		}
+	});
 }
 
 fn run_app() -> Result<(), ()> {
 	// Collect command-line arguments
-	let args: Vec<String> = env::args().collect();
+	let args : Vec<String> = env::args().collect();
 	let drive_path = Path::new(&args[1]);
 	// Set up variables
 	let k: i32;
-	let inq_cmd_blk : [u8; INQ_CMD_LEN as usize] = [INQ_CMD_CODE as u8, 0, 0, 0, INQ_REPLY_LEN as u8, 0];
+	let inq_cmd_blk : [u8; INQ_CMD_LEN as usize] = [INQ_CMD_CODE, 0, 0, 0, INQ_REPLY_LEN, 0];
 	let inq_buffer : [u8; INQ_REPLY_LEN as usize];
 	let sense_buffer : [u8; 32];
 	// Open the file given for desired drive
-    let mut sg = match File::open(&drive_path) {
-        Err(err) => panic!("Could not open {}: {}", drive_path.display(), err),
-        Ok(file) => file,
+	let mut sg = match File::open(&drive_path) {
+		Err(err) => panic!("Could not open {}: {}", drive_path.display(), err),
+		Ok(file) => file,
 	};
 	let sg_fd : i32 = sg.as_raw_fd();
 	// Check if sg device
-	match ioctl(sg_fd, SG_GET_VERSION_NUM, &k) {
-		Err(err) => panic!("{} is not an sg device, or old sg driver", &args[1]),
-        Ok(file) => file,
+	if ioctl(sg_fd, SG_GET_VERSION_NUM, &k) < 0 || k < 30000 {
+		panic!("{} is not an sg device, or old sg driver", &args[1]);
 	}
-	if k < 30000 {
-        panic!("{} is not an sg device, or old sg driver", &args[1]);
-        Err()
-    }
 	// Prepare inquiry command
-	struct sg_io_hdr io_hdr;
-	
-	memset(&io_hdr, 0, sizeof(struct sg_io_hdr));
-    io_hdr.interface_id = 'S';
-    io_hdr.cmd_len = sizeof(turCmbBlk);
-    io_hdr.mx_sb_len = sizeof(sense_b);
-    io_hdr.dxfer_direction = SG_DXFER_NONE;
-    io_hdr.cmdp = turCmbBlk;
-    io_hdr.sbp = sense_b;
-    io_hdr.timeout = DEF_TIMEOUT;
+	let io_hdr = sg_io_hdr {
+		interface_id: 		'S' as i32,
+		dxfer_direction: 	SG_DXFER_FROM_DEV,
+		cmd_len: 			inq_cmd_blk.len() as u8,
+		mx_sb_len: 			sense_buffer.len() as u8,
+		dxfer_len: 			INQ_REPLY_LEN as u32,
+    	dxferp: 			inq_buffer.as_mut_ptr() as *mut c_void,
+		cmdp: 				inq_cmd_blk.as_ptr(),
+		sbp: 				sense_buffer.as_mut_ptr(),
+		timeout: 			20000,
+		..Default::default()
+	};
+	if (ioctl(fd, SG_IO, &io_hdr) < 0) {
 
-    if (ioctl(fd, SG_IO, &io_hdr) < 0) {
 	}
 	Ok(())
 }
